60e090fa5456559739f209bfb07f829b4d7c0c0f
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-24 15:26:24 +0100

    passed test to loop input

--- lib/echo.rb
+++ lib/echo.rb
@@ -2,16 +2,28 @@
 
 class Echo
   def say_something
-    answer
     time = Time.now.strftime('%Y-%m-%d | %H:%M |')
-    print "#{time} You said: '#{@answer}'!"
+    print "#{time} You said: '#{@answer}'!\n"
   end
 
   def prompt
-    print 'Say something:'
+    print "Say something: "
   end
 
   def answer
-    @answer = STDIN.gets
+    @answer = STDIN.gets.chomp
+  end
+
+  def engine
+    loop do
+      prompt
+      answer
+      if @answer == 'exit'
+        print 'Goodbye'
+        break
+      else
+        say_something
+      end
+    end
   end
 end
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -6,7 +6,7 @@ describe Echo do
   subject(:echo) { described_class.new }
   describe '#prompt' do
     it ' prompts a user to say something' do
-      expect { echo.prompt }.to output('Say something:').to_stdout
+      expect { echo.prompt }.to output('Say something: ').to_stdout
     end
   end
 
@@ -17,11 +17,22 @@ describe Echo do
     end
 
     it ' outputs the phrase and time entered' do
-      time = Time.new(2018, 01, 9, 16, 26, 2, '+00:00')
+      time = Time.new(2018, 0o1, 9, 16, 26, 2, '+00:00')
       allow(Time).to receive(:now).and_return(time)
       allow(STDIN).to receive(:gets).and_return('Hello, World')
+      echo.answer
+      expect { echo.say_something }.to output("2018-01-09 | 16:26 | You said: 'Hello, World'!\n").to_stdout
+    end
 
-      expect { echo.say_something }.to output("2018-01-09 | 16:26 | You said: 'Hello, World'!").to_stdout
+    describe '#engine' do 
+      it ' continues to prompt user for input until exit' do
+        time = Time.new(2018, 01, 9, 16, 26, 2, '+00:00')
+        allow(Time).to receive(:now).and_return(time)
+        allow(STDIN).to receive(:gets).and_return('Hello, World')
+
+        allow(echo).to receive(:loop).and_yield
+        expect { echo.engine }.to output("Say something: 2018-01-09 | 16:26 | You said: 'Hello, World'!\n").to_stdout
+      end
     end
   end
 end
