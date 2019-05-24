29e8cc42f65ae9f0caca9844b8d7cad3f2d2eea9
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-24 14:56:21 +0100

    passed test to ouptut time and answer

--- lib/echo.rb
+++ lib/echo.rb
@@ -1,10 +1,17 @@
-class Echo 
+# frozen_string_literal: true
 
-  def say_something(phrase)
-    STDIN.gets(phrase)
+class Echo
+  def say_something
+    answer
+    time = Time.now.strftime('%Y-%m-%d | %H:%M |')
+    print "#{time} You said: '#{@answer}'!"
   end
 
-  def prompt 
-   print "Say something:"
+  def prompt
+    print 'Say something:'
+  end
+
+  def answer
+  @answer = STDIN.gets
   end
 end
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -8,10 +8,19 @@ subject(:echo) {described_class.new}
     end
   end
 
-  describe '#say_something' do 
-    it 'allows a user to enter phrase to say something' do
+  describe '#answer' do 
+    it 'outputs the answer from user input' do
       allow(STDIN).to receive(:gets).and_return('Hello World')
-      expect(echo.say_something('Hello World')).to eq('Hello World')
+      expect(echo.answer).to eq('Hello World')
+    end
+
+    it ' outputs the phrase and time entered' do 
+      time = Time.new(2018, 01, 9, 16, 26, 2, "+00:00")
+      allow(Time).to receive(:now).and_return(time)
+    allow(STDIN).to receive(:gets).and_return('Hello, World')
+
+    expect { echo.say_something }.to output( "2018-01-09 | 16:26 | You said: 'Hello, World'!").to_stdout
+
     end
   end
 end
